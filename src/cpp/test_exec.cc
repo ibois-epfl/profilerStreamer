e55b8b11157dc4ae8877ce4eaf55d9421e2e960b
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-02-18 18:59:01 +0100

    feat-wip: add transformation at test_exec level

--- src/cpp/test_exec.cc
+++ src/cpp/test_exec.cc
@@ -39,7 +39,7 @@ int main()
     });
     opcuaThread.detach();
 
-    std::this_thread::sleep_for(std::chrono::seconds(60)); 
+    std::this_thread::sleep_for(std::chrono::seconds(30)); 
     hasStopped = true;
     std::this_thread::sleep_for(std::chrono::milliseconds(500)); 
 
@@ -51,7 +51,22 @@ int main()
     
     ProfilerStreaming::PostProcess::DataSlicer dataSlicer(profilesOverTime, rangefinderDataOverTime);
     std::pair<double, double> thresholds = dataSlicer.Slice(1000);
+    Eigen::Matrix3d rotationXAxis = Eigen::AngleAxisd(1.5 * M_PI / 180, Eigen::Vector3d::UnitX()).toRotationMatrix();
+    Eigen::Matrix4d firstTransformationMatrix = Eigen::Matrix4d::Identity();
+    firstTransformationMatrix.block<3,3>(0,0) = rotationXAxis;
+    Eigen::Matrix4d transformationMatrix = Eigen::Matrix4d::Identity();
+    double y = -1758.07;
+    double z = 555.30;
+    transformationMatrix(1, 1) = -1;
+    transformationMatrix(0, 0) = -1;
+    transformationMatrix(0, 3) = 4834;
+    transformationMatrix(1, 3) = y-339.63;
+    transformationMatrix(2, 3) = z-362.92;
     std::vector<Eigen::Vector3d> regularizedProfile = dataSlicer.ComputeRegularizedProfiles();
+    for (auto& point : regularizedProfile)
+    {
+        point = (transformationMatrix * firstTransformationMatrix * point.homogeneous()).head<3>();
+    }
     open3d::geometry::PointCloud combinedPointCloud = ProfilerStreaming::SpatialData::PCWT2Open3DConverter::Convert(regularizedProfile);
     std::cout << "Combined point cloud has " << combinedPointCloud.points_.size() << " points." << std::endl;
     open3d::io::WritePointCloud("combined_point_cloud.ply", combinedPointCloud);
