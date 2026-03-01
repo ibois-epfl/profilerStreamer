d43d9d354655fe1910f6e732e0d07762f102cbd6
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-03-01 21:26:33 +0100

    feat-wip: reflect change with slices that are now kept after post-processing

--- src/py/test.py
+++ src/py/test.py
@@ -34,7 +34,7 @@ def main():
     opcua_recorder = psb.OPCUARecorder(opcua_communicator, 5)
     tcp_recorder.StartRecording()
     opcua_recorder.StartRecording()
-    time.sleep(45)
+    time.sleep(240)
     tcp_recorder.StopRecording()
     opcua_recorder.StopRecording()
 
@@ -43,7 +43,7 @@ def main():
     rangefinder_data_over_time = opcua_recorder.GetRecordedData()
     print(f"First profile timestamp: {profiles_over_time[0].GetTimeStampAsInt()}, first rangefinder timestamp: {rangefinder_data_over_time[0].GetTimeStampAsInt()}")
     slicer = psb.DataSlicer(profiles_over_time, rangefinder_data_over_time)
-    mini, maxi = slicer.Slice(1000)
+    mini, maxi = slicer.Slice(2000)
     if len(slicer.GetRangeFinderDistancesSortedIntoSegments()) == 0 or len(slicer.GetProfilesSortedIntoSegments()) == 0:
         print("No valid segments found.")
         return
@@ -62,22 +62,26 @@ def main():
 
 
 if __name__ == "__main__":
-    a = None
+    a = []
     run = True
     if run:
         transform = Rhino.Geometry.Transform.RotationZYX(0, 0, 1.5 / 180 * 3.141592653589793)
         transform.M00 = -1
         transform.M11 = -1
-        transform.M03 = 4834
-        transform.M13 = y - 331
-        transform.M23 = z - 361
+        transform.M03 = 4828
+        transform.M13 = y - 330.0
+        transform.M23 = z - 359.5
         result = []
         n_seg = None
         rh_pc = Rhino.Geometry.PointCloud()
         res = main()
-        for point in res:
-            rh_pt = Rhino.Geometry.Point3d(point[0], point[1], point[2])
-            rh_pt.Transform(transform)
-            rh_pc.Add(rh_pt)
-        a = rh_pc
+        n_seg = len(res)
+        
+        for i, segment in enumerate(res):
+            rh_pc = Rhino.Geometry.PointCloud()
+            for point in segment:
+                rh_pt = Rhino.Geometry.Point3d(point[0], point[1], point[2])
+                rh_pc.Add(rh_pt)
+            rh_pc.Transform(transform)
+            a.append(rh_pc)
     a = a
