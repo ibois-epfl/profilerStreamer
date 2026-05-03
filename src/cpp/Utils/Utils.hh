1c7d5e2b83cc46eb26774d39b9555bea014394dd
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-05-03 21:39:41 +0200

    fix: again fix of sloppy previous commit

--- src/cpp/Utils/Utils.hh
+++ src/cpp/Utils/Utils.hh
@@ -4,8 +4,11 @@
 #include <chrono>
 #include "../Data/Data.hh"
 
+#include <Eigen/Dense>
+
 namespace ProfilerStreaming::Utils
 {
     std::pair<double, double> linearRegressionSlope(const std::vector<ProfilerStreaming::SpatialData::PointCloudWithTimestamp>& data, int referenceIndex);
-}
+
     Eigen::Matrix4d ComputeTransformationMatrix(const std::vector<Eigen::Vector3d>& sourcePoints, const std::vector<Eigen::Vector3d>& targetPoints);
+}
