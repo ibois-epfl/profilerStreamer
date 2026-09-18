2bdf86926697078829079e853503ee285bf09320
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-09-18 10:51:30 +0200

    feat: remove unused py directory [no ci]

--- src/py/README.md
+++
@@ -1,2 +0,0 @@
-## README 
-placeholder for now
--- src/py/profiler_streamer/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from .bindings import profilerStreamerBindings as psb
-
-__all__ = ["psb"]
-
-DLL_PATHS = os.path.join(os.path.dirname(__file__), "bindings")
-os.add_dll_directory(DLL_PATHS)
